0f302e3cd3e00a07a6270a26f3b6dfb26e0699f6
Elizabeth Sarduy <elizabeth_s7@ymail.com>
2020-02-12 10:12:15 -0500

    Updates

---
+++ precache-manifest.1baad132004a79537c3a3f54bd4e6718.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e25f5bf7a8ae4a71b869a10233b6ea7d",
+    "url": "/dig4638-mobile-dev/index.html"
+  },
+  {
+    "revision": "dd3b48cdd04bf03c2821",
+    "url": "/dig4638-mobile-dev/static/css/main.0ebde3ef.chunk.css"
+  },
+  {
+    "revision": "e02cba94be16e740280c",
+    "url": "/dig4638-mobile-dev/static/js/2.3ded2b1a.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/dig4638-mobile-dev/static/js/2.3ded2b1a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dd3b48cdd04bf03c2821",
+    "url": "/dig4638-mobile-dev/static/js/main.12482704.chunk.js"
+  },
+  {
+    "revision": "5a0b291810fb5025ad53",
+    "url": "/dig4638-mobile-dev/static/js/runtime-main.f43191c6.js"
+  }
+]);
