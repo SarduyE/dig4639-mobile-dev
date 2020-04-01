a36f4e888c4dc2c14a577b2a732dac1176a34dff
Elizabeth Sarduy <elizabeth_s7@ymail.com>
2020-03-31 22:17:20 -0400

    Lab 5

--- labs/5/my-app/src/App.js
+++ labs/5/my-app/src/App.js
@@ -1,44 +1,48 @@
 import React from 'react';
 import './App.css';
-import Card from './components/card/index.js'
-let WAPI = 'https://api.weather.gov/gridpoints/MLB/25,69/forecast';
+import Card from  './components/card/index.js';
+const WAPI = 'https://api.weather.gov/gridpoints/MLB/25,69/forecast';
 
 
-class App extends React.Component{
+class App extends React.Component {
   state = {
-    weather: []
+    weather: []    
   }
+  fetchWeather() {
 
-  weather() {
     fetch(WAPI)
+   
     .then(response => response.json())
+  
     .then(data => {
       let periods = data.properties.periods;
       console.log(data)
       this.setState({
         weather: periods
       });
-    });
+    }
+    );
   }
-
-  componentDidMount(){
-    this.weather();
+  componentDidMount() {
+    this.fetchWeather();
   }
-  render(){
+  render() {
     return(
       <>{console.log(this.state.weather)}
-      {(this.state.weather).map(
-        (state, i) =>
-        <Card Key={i}  id={state.number} title={state.name} 
-        content={state.temperature} unit={state.temperatureUnit}
-        descriptio={state.detailedForecast}
-        > </Card>
-      )}
+        {(this.state.weather).map(
+            (state, i) => 
+            <Card
+                key={i}
+                id={state.number}
+                title={state.name}
+                content={state.temperature}
+                unit={state.temperatureUnit}
+                description={state.detailedForecast}
+            ></Card>
+        )}
       </>
-  
-)
+    )
+  }
 }
-}
-
 
 export default App;
--- labs/5/my-app/src/components/card/index.js
+++ labs/5/my-app/src/components/card/index.js
@@ -1,19 +1,16 @@
+
 import React from 'react';
 import './index.css';
 
-
-
 class Card extends React.Component {
-  render(){
-    return(
-      <div className="cards">
-        
-        <h3>{this.props.title}</h3>  <h2>{this.props.content}&deg; {this.props.unit}</h2>
-         <p>{this.props.description}</p>
-      </div>
-
-
-    )
+  render() {
+      return (
+        <div className="card">         
+          <h3>{this.props.title}</h3>
+          <h2>{this.props.content}&deg; {this.props.unit}</h2>
+          <p>{this.props.description}</p>
+        </div>
+      )
   }
 }
 
