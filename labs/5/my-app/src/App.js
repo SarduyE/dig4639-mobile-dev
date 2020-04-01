370ac304982f71a9ee9a46da7325ee55ebd9e33b
Elizabeth Sarduy <elizabeth_s7@ymail.com>
2020-03-31 22:22:06 -0400

    Lab 5

--- labs/5/my-app/src/App.js
+++ labs/5/my-app/src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './App.css';
 import Card from  './components/card/index.js';
-const WAPI = 'https://api.weather.gov/gridpoints/MLB/25,69/forecast';
+let WAPI = 'https://api.weather.gov/gridpoints/MLB/25,69/forecast';
 
 
 class App extends React.Component {
@@ -9,36 +9,25 @@ class App extends React.Component {
     weather: []    
   }
   fetchWeather() {
-
     fetch(WAPI)
-   
     .then(response => response.json())
-  
-    .then(data => {
+   .then(data => {
       let periods = data.properties.periods;
       console.log(data)
       this.setState({
         weather: periods
-      });
-    }
-    );
-  }
+      });  } );
+  } 
   componentDidMount() {
     this.fetchWeather();
-  }
-  render() {
+  } render() {
     return(
       <>{console.log(this.state.weather)}
         {(this.state.weather).map(
             (state, i) => 
             <Card
-                key={i}
-                id={state.number}
-                title={state.name}
-                content={state.temperature}
-                unit={state.temperatureUnit}
-                description={state.detailedForecast}
-            ></Card>
+                key={i} id={state.number} title={state.name}content={state.temperature} unit={state.temperatureUnit}
+                description={state.detailedForecast}/>
         )}
       </>
     )
--- labs/5/my-app/src/components/card/index.js
+++ labs/5/my-app/src/components/card/index.js
@@ -6,8 +6,7 @@ class Card extends React.Component {
   render() {
       return (
         <div className="card">         
-          <h3>{this.props.title}</h3>
-          <h2>{this.props.content}&deg; {this.props.unit}</h2>
+          <h3>{this.props.title}</h3> <h2>{this.props.content}&deg; {this.props.unit}</h2>
           <p>{this.props.description}</p>
         </div>
       )
